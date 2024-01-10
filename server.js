162b80182f9100f7a00e5b5f4e345ef8c51b9e7d
crhbala <chrisbala8888@gmail.com>
2024-01-10 16:21:54 +0530

    create a logger file to change the console.log to info and console.log to error

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const { log } = require('console');
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-const config = require('./utils/config')
+const config = require('./utils/config');
+const{info, error} = require('./utils/logger')
 
 // middleware
 app.use(express.json());
@@ -10,13 +11,13 @@ app.use(express.json());
 // connect to the database
 
 
-console.log('Connecting to DB....');
+info('Connecting to DB....');
 mongoose.connect(config.MONGODB_URI)
     .then(() => {
-        console.log('Connected to mongoDB');
+        info('Connected to mongoDB');
     })
     .catch((error) => {
-        console.log('Error connecting to MongoDB', error);
+        error('Error connecting to MongoDB', error);
     });
 
 //define a schema
@@ -42,5 +43,5 @@ app.get('/api/notes', (request, response) => {
 //MAKE THE SERVER TO LISTEN FOR HTTP REQUESTS
 
 app.listen(config.PORT, () => {
-    console.log(`server running at http://${config.HOSTNAME}:${config.PORT}`);
+    info(`server running at http://${config.HOSTNAME}:${config.PORT}`);
 });
