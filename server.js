35ae4e588a62a97ed0b4a5b945c4ff33e8fa8342
crhbala <chrisbala8888@gmail.com>
2024-01-10 19:55:51 +0530

    cors install and implement(used to allowrequests from other origins than the server

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const config = require('./utils/config');
-const{info, error} = require('./utils/logger')
+const { info, error } = require('./utils/logger');
+const cors = require('cors')
 
 // middleware
+app.use(cors());
 app.use(express.json());
 
 // connect to the database
